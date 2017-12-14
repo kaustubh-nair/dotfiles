9898f3d69cb4bc822632ce59f7493c422b47abcd
Kaustubh Nair <kaustubh.nair@iiitb.org>
2017-12-14 13:03:12 +0000

    modifications

--- .local/share/Trash/files/howler.3/howler_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .import views
-
-urlpatterns = [
-       # url(r'^$',views.homepage,name='home'),
-       url(r'^$',views.all_users),
-       url(r'^(?P<user_id>[0-9]+)/$',views.view_user,name = 'view_user'),
-        ]
