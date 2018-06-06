80872aa8aa8778a186f1f7a3cd18188624fc9277
Kaustubh Nair <kaustubh.nair108@gmail.com>
2018-06-06 20:56:21 +0530

    first commit

---
+++ new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp ./.vimrc ~/.vimrc
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y zsh
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -
+
+
+
+
